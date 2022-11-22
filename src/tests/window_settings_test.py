45a836099f27c4633a9fd7bb13a1dd645216c95c
t0ffe <toffelaakso@gmail.com>
2022-11-22 22:16:47 +0200

    added initial window setting tests WIP

---
+++ src/tests/window_settings_test.py
@@ -0,0 +1,24 @@
+import unittest
+from ui.myyja_view import *
+from ui.opiskelija_view import *
+from ui.user_select import UserSelect
+
+from tkinter import Tk
+from ui.ui import UI
+
+class TestGeneralWindowSettings(unittest.TestCase):
+
+    def setUp(self):
+        self.window = Tk()
+        self.window.title("Merkkien hallinta sovellus test")
+        self.window.geometry("500x500")
+
+        self.ui_view = UI(self.window)
+        self.ui_view.start()
+        print(self.ui_view)
+
+    def test_window_title(self):
+        self.assertEqual(self.window.title() ,"Merkkien hallinta sovellus test")
+
+    def test_window_size(self):
+        self.assertEqual(self.window.size() ,(0,0)) # beacause window is neve spawned so it's size is 0,0
